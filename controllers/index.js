d64fd7af8b71c1fda81ef101748eba40c5f7d456
LinhBui88 <87442413+Linhbui88@users.noreply.github.com>
2021-10-25 18:22:03 -0400

    fisnished fetchAPI

--- controllers/index.js
+++ controllers/index.js
@@ -1,24 +1,46 @@
 const router = require('express').Router()
 const blogPosts= require('../models/blogPosts')
- 
+var slugify = require('slugify')
 
 router.get('/', (req, res) => res.render('index',{
   name: 'Linh',
   date: new Date(),
   img_src:'/img/Yoona.jpg'
 }))
+
 router.get('/about',(req,res) => res.render('about'))
+
 router.get('/contact', (req, res) => res.render('contact'))
+
 router.get('/services',(req,res) => res.render('services'))
-router.get('/blog',(req,res) => res.render('blog',
-  { blogPosts}))
+
+router.get('/blog', (req, res) => {
+  res.render('blog', {
+    post: blogPosts.map(post => {
+      const slug = slugify(post.name, { lower: true })
+      post.slug = slug
+      return post
+    })
+  })
+})
+
 router.get('/blog/:slug',(req,res) => {
   const postObj = blogPosts
   .find(post => req.params.slug === post.slug)
   console.log(postObj)
   res.render('post',postObj)
 
+
   })
+router.get('/add-post',(req,res) =>res.render('add-post'))
+
+router.post('/add-post',(req,res) => {
+ const body = req.body
+ body.date = new Date()
+ blogPosts.push(body)
+  res.status(200).send('Post added')
+
+})
 
 
 
--- models/blogPosts.js
+++ models/blogPosts.js
@@ -1,6 +1,3 @@
-
-const { default: slugify } = require('slugify')
-const slug= require('slugify')
 const blogPosts = [
   {
     name: '7 eCommerce Design Trends You Need to Know for 2022',
@@ -41,10 +38,5 @@ const blogPosts = [
     content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis doloremque at iusto quam. Nobis repellendus placeat ea consectetur architecto minima, corporis aliquam enim error nesciunt, exercitationem perferendis excepturi, atque tempora quos laudantium. Nisi explicabo magni quae! Laboriosam ipsa, magni fuga dicta quae voluptatem quam. Illo quo voluptatum omnis. Explicabo, laboriosam? Dolores ea ipsum, iste non earum nobis officia dignissimos, adipisci quibusdam placeat ipsa culpa quisquam necessitatibus enim incidunt eaque sequi suscipit. Voluptatem necessitatibus accusantium expedita totam velit officia amet, similique modi porro impedit, adipisci sed. Porro odio tenetur, suscipit recusandae sequi itaque deserunt harum libero. Aut error sapiente dolorem ratione.'  
   
   }, 
-].map (post => {
-  const slug = slugify(post.name,{lower:true})
-  post.slug= slug
-  return post
-})
-
+]
 module.exports = blogPosts
